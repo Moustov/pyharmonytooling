7145579886e4bb0ebc857d92fd83f2bf4f04fde6
chris <christophe.moustier@free.fr>
2022-11-23 08:16:12 +0100

    intervals + __eq__ between notes

--- src/harmony/note.py
+++ src/harmony/note.py
@@ -1,7 +1,10 @@
 class Note:
     CHROMATIC_SCALE = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-    TEMPERED_CHROMATIC_SCALE = ["Ab", "A", "A#", "Bb", "B", "B#", "Cb", "C", "C#", "Db", "D", "D#", "Eb", "E", "E#",
-                                "Fb", "F", "F#", "Gb", "G", "G#"]
+    TEMPERED_CHROMATIC_SCALE = {"Ab": 1, "A": 1, "A#": 0, "Bb": 1, "B": 0,
+                                "Cb": 1, "C": 1, "C#": 0, "Db": 1, "D": 1, "D#": 0,
+                                "Eb": 1, "E": 0, "Fb": 1, "F": 1, "F#": 0,
+                                "Gb": 1, "G": 1, "G#": 0}
+    # see https://en.wikipedia.org/wiki/Comma_(music) to make it generic as much as possible
     EXTENDED_CHROMATIC_SCALE = ["Abb", "Ab", "A", "A#", "A##", "Bbb", "Bb", "B", "B#", "B##",
                                 "Cbb", "Cb", "C", "C#", "C##", "Dbb", "Db", "D", "D#", "D##",
                                 "Ebb", "Eb", "E", "E#", "E##", "Fbb", "Fb", "F", "F#", "F##",
@@ -9,6 +12,10 @@ class Note:
 
     def __init__(self, name):
         self.name = name
+        if name == "B#":
+            self.name = "C"
+        if name == "E#":
+            self.name = "F"
 
     def __eq__(self, other) -> bool:
         """
@@ -18,18 +25,33 @@ class Note:
         """
         if type(other) == str and self.name == other:
             return True
-        if type(other) == Note and self.name == other.name:
+        elif type(other) == Note and self.name == other.name:
             return True
-        if "#" or "b" in self.name:
-            index_other = -1
-            index_name = Note.TEMPERED_CHROMATIC_SCALE.index(self.name)
-            if type(other) == Note:
-                index_other = Note.TEMPERED_CHROMATIC_SCALE.index(other.name)
-            if type(other) == str:
-                index_other = Note.TEMPERED_CHROMATIC_SCALE.index(other)
-            same = (index_name == index_other + 1) \
-                    or (index_other == index_name + 1) \
-                    or (index_other == len(Note.TEMPERED_CHROMATIC_SCALE) - 1 and index_name == 0) \
-                    or (index_other == 0 and index_name == len(Note.TEMPERED_CHROMATIC_SCALE) - 1)
+        distance = self.get_interval_in_half_tones(other)
+        return distance == 0
 
-            return same
+    def get_interval_in_half_tones(self, other) -> int:
+        """
+        return the number of half tones between self & other
+        :type note: Note
+        :param note:
+        :return:
+        """
+        tempered_scale = list(Note.TEMPERED_CHROMATIC_SCALE.keys())
+        index_name = tempered_scale.index(self.name)
+        if type(other) == Note:
+            index_other = tempered_scale.index(other.name)
+        elif type(other) == str:
+            index_other = tempered_scale.index(other)
+        nb_half_tones = 0
+        if index_other < index_name:
+            for n in tempered_scale[index_other: index_name]:
+                nb_half_tones += Note.TEMPERED_CHROMATIC_SCALE[n]
+            nb_half_tones -= 12
+        else:
+            for n in tempered_scale[index_name:index_other]:
+                nb_half_tones += Note.TEMPERED_CHROMATIC_SCALE[n]
+        if nb_half_tones >= 0:
+            return nb_half_tones % 12
+        else:
+            return nb_half_tones % -12
--- test/1_objects_tests/test_note.py
+++ test/1_objects_tests/test_note.py
@@ -16,3 +16,33 @@ class TestNote(TestCase):
         assert Note("C#") == "Db"
         # limits
         assert Note("G#") == "Ab"
+
+    def test_get_interval_c_e(self):
+        c = Note("C")
+        e = Note("E")
+        assert c.get_interval_in_half_tones(e) == 4
+
+    def test_get_interval_e_c(self):
+        c = Note("C")
+        e = Note("E")
+        assert e.get_interval_in_half_tones(c) == -8
+
+    def test_get_interval_c_g(self):
+        c = Note("C")
+        g = Note("G")
+        assert c.get_interval_in_half_tones(g) == 7
+
+    def test_get_interval_cs_es(self):
+        cs = Note("C#")
+        es = Note("E#") # the get_interval() should translate this into F
+        assert cs.get_interval_in_half_tones(es) == 4
+
+    def test_get_interval_gs_ab(self):
+        gs = Note("G#")
+        ab = Note("Ab") # the get_interval() should translate this into F
+        assert gs.get_interval_in_half_tones(ab) == 0
+
+    def test_get_interval_ab_gs(self):
+        gs = Note("G#")
+        ab = Note("Ab") # the get_interval() should translate this into F
+        assert ab.get_interval_in_half_tones(gs) == 0
